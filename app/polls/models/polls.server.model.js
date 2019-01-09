ccd39b9a4bf11e5e21bbb646fc67ac830727e58e
Shajan Jacob <shajanjp@gmail.com>
2019-01-09 19:21:09 +0530

    adds poll mode

--- app/polls/models/polls.server.model.js
+++ app/polls/models/polls.server.model.js
@@ -3,6 +3,22 @@ const Schema = mongoose.Schema;
 
 let pollSchema = new Schema({
   title: String,
+  description: String,
+  coverImage: String,
+  feedbacks: [{
+    title: String,
+    users: [{
+      type: Schema.ObjectId,
+      ref: 'user'
+    }]
+  }],
+  createdBy: {
+    type: Schema.ObjectId,
+    ref: 'user'
+  },
+  createdDate: Date, 
+  updateDate: Date,
+  expiryDate: Date,
 });
 
 mongoose.model('poll', pollSchema);
