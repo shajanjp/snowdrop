3a7959b1c66b8a4305e6e0529a7fc90148bde6bf
Shajan Jacob <shajanjp@gmail.com>
2019-01-14 13:15:12 +0530

    adds mongQL controller and route

---
+++ app/common/controllers/monql.server.controller.js
@@ -0,0 +1,68 @@
+require('../../users/models/users.server.model.js');
+require('../../polls/models/polls.server.model.js');
+const mongoose = require('mongoose');
+const User = mongoose.model('user');
+const Poll = mongoose.model('poll');
+
+function chooseModel(modelName){
+  switch(modelName) {
+    case "users":
+      console.log('users model selected');
+      return User;
+      break;
+    case "polls":
+      console.log('polls model selected');
+      return Poll;
+      break;
+    default:
+  }
+}
+
+function makeSelectObjectFromArray(selectList){
+  let selectObject = {};
+  selectList.forEach((keyName) => {
+    selectObject[keyName] = 1;
+  })
+  console.log('selectObject', selectObject);
+  return selectObject;
+}
+
+function doTask(Model, taskDetails){
+  switch(taskDetails.method){
+    case "CREATE":
+      console.log('method CREATE');
+      let newModelData = new Model(req.query.data);
+      return newModelData.save();
+      break;
+    case "READ":
+      return Model.find(taskDetails.filter, makeSelectObjectFromArray(taskDetails.select))
+      .populate({ 'path': taskDetails.populate[0].path, 'select': makeSelectObjectFromArray(taskDetails.populate[0].select) })
+      .skip(taskDetails.skip)
+      .limit(taskDetails.limit)
+      .lean();
+      break;
+    default:
+      return Promise.resolve({}); 
+  }
+}
+
+const monqlAPIController = (req, res) => {
+  console.log('req.body.collection', req.body.collection);
+  let Model = chooseModel(req.body.collection);
+  doTask(Model, req.body)
+  .then(taskResult => {
+    res.json({
+      "status": "success",
+      "data": taskResult
+    })
+  })
+  .catch(error => {
+    res.status(500).json({
+      "status": "failed"
+    })
+  })
+} 
+
+module.exports = {
+  monqlAPIController
+}
--- app/common/routes/common.server.api.route.js
+++ app/common/routes/common.server.api.route.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const monqlController = require('../controllers/monql.server.controller.js'); 
+
+router.post('/monql', monqlController.monqlAPIController);
 
 module.exports = router;
