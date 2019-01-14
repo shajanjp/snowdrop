950e0821ede31b5fec72feeccfed7bc792ae6a8e
Shajan Jacob <shajanjp@gmail.com>
2019-01-14 17:20:14 +0530

    adds data validation

--- app/common/controllers/monql.server.controller.js
+++ app/common/controllers/monql.server.controller.js
@@ -1,8 +1,48 @@
 require('../../users/models/users.server.model.js');
 require('../../polls/models/polls.server.model.js');
 const mongoose = require('mongoose');
+const joi = require('joi');
 const User = mongoose.model('user');
 const Poll = mongoose.model('poll');
+
+const mongoId = joi.string().length(24);
+
+const populateBodySchema = joi.object().keys({
+  path: joi.string().required(),
+  select: joi.array().items(joi.string()).optional().default([])
+});
+
+const optionsBodySchema = joi.object().keys({
+  safe: joi.boolean().optional()
+});
+
+const requestBodySchema = joi.object().keys({
+    "method": joi.string().required().default('READ'),
+    "collection": joi.string().required().default('users'),
+    "data": joi.object().optional().default({}),
+    "filter": joi.object().optional().default({}),
+    "populate": joi.array().items(populateBodySchema).optional(),
+    "select": joi.array().items(joi.string()).optional().default([]),
+    "skip": joi.number().optional().default(0),
+    "limit": joi.number().optional(),
+    "options": optionsBodySchema
+});
+
+const validateRequestBody = function(req, res, next) {
+  joi.validate(req.body, requestBodySchema, {'stripUnknown': true}, function(err, validated) {
+    if (err) {
+      return res.status(500).json({
+        'errors': err.details[0].message,
+      });
+    } else {
+      res.locals.dataBody = validated;
+      if(res.locals.dataBody.method == 'UPDATE' || res.locals.dataBody.method == 'DELETE'){
+        res.locals.dataBody.filters = { createdAt: res.locals.authUserId };
+      }
+      return next();
+    }
+  });
+};
 
 const modelList = {
   users: User,
